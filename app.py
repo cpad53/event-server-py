fcb7324a27cb3465ef70259c04e7cae5943871d3
Praveen G Shirali <praveengshirali@gmail.com>
2022-05-02 19:52:02 +0530

    Moved FileStore into the invocation script, such that the application is a single file.

--- app.py
+++ app.py
@@ -7,9 +7,27 @@ from flask import Response
 import json
 import os
 
-from src.store import FileStore
-
 app = Flask(__name__)
+
+
+class FileStore:
+    """Manages the backend store for event messages."""
+
+    def __init__(self, fname):
+        self._file = fname
+
+    def store_event(self, event):
+        """Store the received event in the store"""
+        with open(self._file, 'a') as f:
+            f.write("{}\n".format(event))
+
+    def get_last_event(self):
+        """Return the last stored event"""
+        with open(self._file) as f:
+            l = f.readlines()
+        if len(l):
+            return l[-1]
+
 
 def handle_errors(func):
     """A decorator which handles failures in the HTTP controllers.
